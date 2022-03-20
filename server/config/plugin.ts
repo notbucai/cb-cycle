d64640275c08835e34b16773692f92f75e827304
bucai <1450941858@qq.com>
2022-03-20 10:56:21 +0800

    fix: clear env data

--- server/config/plugin.ts
+++ server/config/plugin.ts
@@ -1,11 +1,10 @@
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
-  // static: true,
-  // nunjucks: {
-  //   enable: true,
-  //   package: 'egg-view-nunjucks',
-  // },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  }
 };
 
 export default plugin;
